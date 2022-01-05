4cd46c56d08592bf5d2b84fb35f9d96776c94d17
qbclub <grachevrv@ya.ru>
2022-01-05 13:34:45 +0400

    add aspect-ratio

--- client/src/components/feed.vue
+++ client/src/components/feed.vue
@@ -6,7 +6,7 @@
         :key="index"
         class=" col-12 col-md-4"
       >
-      <v-img v-if="photo.image"  :src="addImage(photo.image)"></v-img>
+      <v-img aspect-ratio="1" v-if="photo.image"  :src="addImage(photo.image)"></v-img>
     
        <p class="text-center text-h6">{{ photo.name }}</p> 
        <p class="text-right">{{ photo.author }}</p> 
